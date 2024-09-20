8286364619a03018138f3cd7c03f6fbc7ab95b7b
kijung <kijung991008@naver.com>
2024-09-20 17:32:36 +0900

    2024.09.20

---
+++ src/apis/dto/request/tool/post-tool.request.dto.ts
@@ -0,0 +1,8 @@
+import { NumberLiteralType } from "typescript";
+
+// interface: post tool request Body dto //
+export default interface PostToolRequestDto {
+    name: string;
+    purpose: string;
+    count: number;
+}
