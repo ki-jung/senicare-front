7ebe4ab0c8bd882e3812dcae48d0064969e74213
kijung <kijung991008@naver.com>
2024-09-30 09:01:59 +0900

    2024.09.30

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,13 @@
 import Tool from "./tool.interface";
-import SignInUser from "./sign-in-user.interface";
+import Nurse from "./nurse.interface";
 import Customer from "./customer.interface";
+import CareRecord from "./care-record.interface";
+import SignInUser from "./sign-in-user.interface";
 
 export type {
     Tool,
-    SignInUser,
-    Customer
+    Nurse,
+    Customer,
+    CareRecord,
+    SignInUser
 }
